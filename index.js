f3a4b64498d7855572fb33701390b3e3779a4873
Avishkar <avishkar.kakade7@gmail.com>
2023-03-09 02:22:26 +0530

    port change

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const connectToMongo = require("./db");
 const express = require("express");
 const { json} = require("express");
 const cors=require('cors');
+const port = process.env.PORT || 80;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,4 +12,4 @@ app.get("/", (req, res) => {
 });
 app.use('/auth',require('./routes/auth'));
 app.use('/notes',require('./routes/notes'));       // Routes Using express Router 
-app.listen(80, () => {});
+app.listen(port, () => {});
